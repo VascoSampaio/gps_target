1e634cedfc257b6716b575b8acee109c0111a3fe
Miguel Malaca <miguelmmalaca@tecnico.ulisboa.pt>
2020-04-23 15:49:40 +0100

    Debug Mode working with parameter in roslaunch

--- include/gps_target/gps_target.h
+++ include/gps_target/gps_target.h
@@ -23,6 +23,8 @@
 #include <multidrone_kml_parser/geographic_to_cartesian.hpp>
 #include <geometry_msgs/Point32.h>
 
+
+
 // enum general_configuration{SBAS, RATE, VALSET, MSG, CFG, PRT, DGNSS, NAV, UART};
 // enum messages_configutation{GNS, GLL, GSA, GSV, RMC, VTG,  GGA};
 
@@ -33,6 +35,8 @@ unsigned char*  nmeaPtr = nmeaBuffer;
 unsigned char*  ubxPtr = ubxBuffer;
 uint8_t         buf_size = 80;
 uint8_t         count_cfg = 0;
+bool            configured = false;
+
 
 
  /* GNS-GPS*/
@@ -75,11 +79,36 @@ public:
   int                                                   keyValue;
   boost::variant<unsigned char, uint16_t, int>          idValue;  
   std::string                                           item;  
+  std::string                                           Print();
 };
 
 // static void ubx_checksum(const unsigned char *data, unsigned len, unsigned char ck[2], unsigned char comparator[2] = {0,0});
 void write_for_checking(unsigned char *buf);
 void ubx_cfg(int fd, ubx_payload_valset* valset);
 std::map<int,ubx_payload_valset*> valset_map ;  	
+
+ubx_payload_valset DGNSSTO{0x201100c4, (unsigned char) 20,"DGNSSTO"};
+ubx_payload_valset S_BAS{0x10930012,   (unsigned char) 0,"SBAS"};
+ubx_payload_valset GPS_ONLY{0x10930025,(unsigned char) 0,"GPS_ONLY"}; 
+ubx_payload_valset rate;
+  //FOR USB port
+
+ubx_payload_valset NMEA_OUT;
+ubx_payload_valset UBX_OUT{0x10780001, (unsigned char)1,"UBX_OUT"};
+ubx_payload_valset NMEA_IN{0x10770002,(unsigned char) 0,"NMEA_IN"}; 
+ubx_payload_valset COMM_OUT{0x20910352,(unsigned char)1, "COMM_OUT"};
+ubx_payload_valset GNS{0x209100b8,(unsigned char)1,"GNS"};
+ubx_payload_valset GLL{0x209100cc,(unsigned char)0,"GLL"}; 
+ubx_payload_valset GSA{0x209100c2,(unsigned char)0,"GSA"};
+ubx_payload_valset GSV{0x209100c7,(unsigned char)0,"GSV"};
+ubx_payload_valset RMC{0x209100ae,(unsigned char)0,"RMC"};
+ubx_payload_valset VTG{0x209100b3,(unsigned char)0,"VTG"};
+ubx_payload_valset GGA{0x209100bd,(unsigned char)0,"GGA"};
+ubx_payload_valset RTCM{0x10770004,(unsigned char) 1,"RTCM"};
+ubx_payload_valset baudrate;
+
+// ubx_payload_valset RTCM_IN{0x10770004,1,"RTCM_IN"};
+// ubx_payload_valset RXM{0x2091026b,0,"RXM"};
+// ubx_payload_valset NAV{0x20910348,0,"NAV"};
 
 #endif
